f6e22658e3430abaef4e2fa20d88c0d04802f50a
ervintandjung <ervintandjung27@gmail.com>
2026-06-30 07:48:39 +0800

    New APK

--- android/app/src/main/java/com/vinzstore/app/MainActivity.java
+++ android/app/src/main/java/com/vinzstore/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.vinzstore.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.OnBackPressedCallback;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                WebView webView = bridge.getWebView();
+
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    finish();
+                }
+            }
+        });
+    }
+}
